6e0348f1c6ea2b08067fe23338e6802a6474bde3
sekerahmet <ahmetseker@gmail.com>
2026-09-21 21:03:13 +0300

    veri_15  TERS=False geri -- PAD'in katkisi TEK BASINA olculsun.

--- deneme2/model_15/veri_15.py
+++ deneme2/model_15/veri_15.py
@@ -29,14 +29,8 @@ from model_15 import Yol, LR, WD, lr_ver
 
 ENB = 500                   # toplananlar 0..500
 HA, HC = 3, 4               # toplanan 3 hane, cevap 4 hane
-TERS = True                 # birler ONCE.  OLCULDU (21 Eylul, rakam tokenli):
-                            #   TERS=False'ta kosullu dogruluk
-                            #     onlar  0,2002   <- elde BIRLERDEN gelir
-                            #                        ama birler HENUZ uretilmedi
-                            #     birler 0,8734   <- en son uretiliyor, bilgi TAM
-                            #   Yani model bilgisi olan basamagi yapiyor.
-                            #   TERS=True her basamaga zaten hesaplanmis
-                            #   bilgiyi verir: elde soldan saga, uretimle AYNI yon.
+TERS = False                # rakam sirasi -- KARAR.  TERS ve PAD ayni anda
+                            # degistirilirse katkilari ayirt edilemez.
 
 ARTI, ESIT, PAD = 10, 11, 12
 N = 13
